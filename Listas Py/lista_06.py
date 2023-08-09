a2943adbdfd44246f6bbf65d2031d5a8c2c94369
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 08:21:53 -0300

    Lista e loop while.

---
+++ Listas Py/lista_06.py
@@ -0,0 +1,9 @@
+print("\nLoop while em lista:")
+fruits = ["Apple", "Mango", "Banana", "Peach"]
+i = 0
+while i < len(fruits):
+ print(fruits[i])
+ i = i + 1
+
+print("Fim do programa")
+print("\n")
