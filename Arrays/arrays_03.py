4b373a145fd659c6c7de460bbc2d2a60f914d952
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 09:54:56 -0300

    Arrays usando Numpy.

--- Arrays/arrays_03.py
+++ Arrays/arrays_03.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-print('\nArrays usando Nunpy:')
+print('Instale a lib Numpy!')
+
+print('\nArrays usando Numpy:')
 array_2 = np.array(["numbers", 3, 6, 9, 12])
 print (array_2)
 print(type(array_2))
