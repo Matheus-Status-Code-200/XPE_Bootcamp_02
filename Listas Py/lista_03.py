12c91ed3a223fc103abae7491260330bb036a737
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 08:11:43 -0300

    Loop for com range

---
+++ Listas Py/lista_03.py
@@ -0,0 +1,8 @@
+#range(start, stop, step)
+
+print("\nLoop for com range:\n")
+fruits = ["Apple", "Mango", "Banana", "Peach"]
+
+for i in range(len(fruits)):
+ print("The list at index", i, "contains a", fruits[i])
+print("\n")
