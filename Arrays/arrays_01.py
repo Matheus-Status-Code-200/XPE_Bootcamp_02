8d5194bfa6cc02a7a98ad8e7d62a29cbe8abb2d9
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 08:25:14 -0300

    Entendendo Arrays.

---
+++ Arrays/arrays_01.py
@@ -0,0 +1,11 @@
+print("\nEstruturas de Arrays:")
+temperaturaMedia = []
+temperaturaMedia.append(31.9)
+temperaturaMedia.append(35.4)
+temperaturaMedia.append(26.4)
+temperaturaMedia.append(24.3)
+temperaturaMedia.append(22.9)
+for i in temperaturaMedia:
+ print(i)
+
+print("\n")
