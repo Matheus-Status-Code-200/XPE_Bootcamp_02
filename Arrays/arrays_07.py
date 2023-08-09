6197a8ab477e1f0c51379923551f615e15dcb245
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 10:33:07 -0300

    Arrays multidimensionais.

---
+++ Arrays/arrays_07.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+print('\nArrays multidimensionais:\n')
+arr_1D = np.array([2,4,6,8])
+print("Array 1D") 
+print(arr_1D)
+print('\n')
+arr_2D = np.array([[0, 1], [1, 0]])
+print ("Array 2D")
+print(arr_2D)
+print('\n')
+arr_3D = np.array([[[0, 1, 1], [1, 0, 1], [1, 1, 0]]])
+print("Array 3D")
+print(arr_3D)
