4c816ba217ca1a76765f6d94ec768e3166eceb2b
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 10:36:58 -0300

    Estrutura de Dados em Pilha.

---
+++ Pilhas/pilhas_01.py
@@ -0,0 +1,13 @@
+print("\nEstrutura de Dados em Pilha:\n")
+
+pilha = [1, 2, 3, 4, 5]
+print("Pilha: ", pilha)
+pilha.append(8)
+print("Inserindo um elemento: ", pilha)
+pilha.append(13)
+print("Inserindo outro elemento: ", pilha)
+pilha.pop()
+print("Removendo um elemento: ", pilha)
+pilha.pop()
+print("Removendo outro elemento: ", pilha)
+print('\n')
