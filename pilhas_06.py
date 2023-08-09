762a4319eeb6cbe0a7080eecbcde4aa47fd99319
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 12:09:12 -0300

    Removendo elementos da fila com queue.

---
+++ pilhas_06.py
@@ -0,0 +1,20 @@
+import queue
+#criando a fila
+fila = queue.Queue()
+#adicionando elementos
+fila.put('Elemento 1')
+fila.put('Elemento 2')
+fila.put('Elemento 3')
+#obtendo o tamanho da fila
+tamanho = fila.qsize()
+print("Tamanho da Fila:", tamanho)
+#removendo elementos
+from time import sleep
+print("Removendo elementos da fila:")
+sleep(1)
+while not fila.empty():
+    print(fila.get())
+    sleep(0.8)
+#verificando se a fila esta vazia
+esta_vazia = fila.empty()
+print("A fila esta vazia?", esta_vazia)
