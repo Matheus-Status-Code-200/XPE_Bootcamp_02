da918ef9e632d89f0343a1f29367ea7245837fc1
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 10:10:10 -0300

    Removendo Elementos no Array

---
+++ Arrays/arrays_05.py
@@ -0,0 +1,9 @@
+print('\nRemovendo Elementos no Array.\n')
+lista = ['A', 'S', 'C', 'E', 'N', 'D', 'E', 'R']
+print("Lista original: ", lista)
+lista.remove("S")
+print("Removendo um elemento: ", lista)
+print("\nLimpando a lista: ")
+lista.clear()
+print('Lista limpa: '+ str(lista))
+print('\n')
