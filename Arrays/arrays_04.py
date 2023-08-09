c14d6821d8eac7da00e2aef4267c1628320d9915
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-09 10:05:06 -0300

    Usando LISTAS como ARRAYS.

---
+++ Arrays/arrays_04.py
@@ -0,0 +1,8 @@
+print('\nLISTAS como ARRAYS:')
+lista2=['L', 'U', 'Z', 'E', 'S']
+lista = ['S', 'C', 'E', 'N', 'D', 'E']
+print("Lista original: ", lista)
+lista.insert(0, "A")
+lista.append("R")
+lista.extend(lista2)
+print("Inserindo elementos: ", lista)
