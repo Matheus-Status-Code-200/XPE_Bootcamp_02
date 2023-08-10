6e651d5f80c476035eeec72c18ebc9ca98c76d64
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-10 20:18:00 -0300

    Conjuntos distintos.

---
+++ Conjuntos/conjuntos_01.py
@@ -0,0 +1,5 @@
+print("\nConjuntos números distintos:")
+numeros = [1, 2, 2, 3, 3, 3]
+numeros_distintos = set(numeros)
+print("Números: ", numeros)
+print("Números distintos: ", numeros_distintos,'\n')
