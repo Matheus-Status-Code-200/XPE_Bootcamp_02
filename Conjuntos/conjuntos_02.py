c28b5119aa06294e30bce6706058201e45a29b07
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-10 20:21:08 -0300

    Adicionao elementos ao conjunto.

---
+++ Conjuntos/conjuntos_02.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 2, 3, 3, 3]
+numeros_distintos = set() 
+print('\nAdicionando elementos ao conjunto:')
+for num in numeros:
+ numeros_distintos.add(num) 
+print("Números: ", numeros)
+print("Números distintos: ", numeros_distintos, '\n')
