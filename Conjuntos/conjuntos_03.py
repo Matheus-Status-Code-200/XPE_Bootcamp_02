57afc780d0500435eae51f107deb7045abea9826
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-14 17:01:41 -0300

    Removendo elementos.

---
+++ Conjuntos/conjuntos_03.py
@@ -0,0 +1,7 @@
+lista = [1, 2, 2, 3, 3, 3]
+nums = set([1, 2, 2, 3, 3, 3])
+print('\nRemovendo elementos de um set:\n')
+print("Números: ", nums)
+nums.remove(2)
+print("Lista: ", lista)
+print("Números: ", nums,'\n')
