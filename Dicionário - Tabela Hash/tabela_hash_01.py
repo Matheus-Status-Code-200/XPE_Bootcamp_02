08f8116835d3755bbc4797e91592f214607cef44
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-15 16:39:22 -0300

    Colisão em Hashes.

---
+++ Dicionário - Tabela Hash/tabela_hash_01.py
@@ -0,0 +1,10 @@
+print('\nTabela Hash: ')
+tabela_hash = {}
+
+# adicionar elementos à tabela
+tabela_hash["chave_1"] = "valor_1"
+tabela_hash["chave_2"] = "valor_2"
+
+# acessar elementos da tabela
+valor_1 = tabela_hash["chave_1"]
+print(valor_1,'\n')
---
+++ Dicionário - Tabela Hash/tabela_hash_02.py
@@ -0,0 +1,5 @@
+print('\nColisão em Hashe: ')
+print('Os dados são armazenados em pares chave-valor.\n')
+country_code = {'25': 'USA' ,'20': 'India', '10': 'Nepal'}
+print (country_code['10'])
+print (country_code['20'] ,'\n')
